d123159f54776f6c0de6005ebc9c3e73eb801e4c
Shamil <belka84083@gmail.com>
2026-07-31 22:11:38 +0300

    refactor: use native HTML template and fix reduce return value based on mentor feedback

--- homework-10.js
+++ homework-10.js
@@ -1,31 +1,34 @@
 import { productList } from './products.js';
 
 const commonObject = productList.reduce((acc, item) => {
-  acc[item.name] = item.description;
+  acc.push({
+    [item.name]: item.description
+  });
+
   return acc;
-}, {});
+}, []);
 
 const createCardTemplate = (product) => {
-  return `
-    <div class="product-card">
-    <img class="product-card__image" src="img/${product.image}.png" alt="Товар ${product.name}">
-    <span class="product-card__category">${product.category}</span>
-    <h2 class="product-card__title">${product.name}</h2>
-    <p class="product-card__description">${product.description}</p>
-    <span class="product-card__composition-title">Состав: </span>
-    <ul class="product-card__composition-list">
-        <li class="product-card__composition-item">
-          ${product.composition.join('</li><li class="product-card__composition-item">')}
-        </li>
-      </ul>
-      <div class="product-card__price-box">
-        <b class="product-card__price-label">Цена</b>
-        <span class="product-card__price-value">${product.price} ₽</span>
-      </div>
+  const template = document.querySelector('#product-template');
+  const cardClone = template.content.cloneNode(true);
 
-    </div>
-  `;
-}
+  cardClone.querySelector('.product-card__image').src = `img/${product.image}.png`;
+  cardClone.querySelector('.product-card__image').alt = `Товар ${product.name}`;
+  cardClone.querySelector('.product-card__category').textContent = product.category;
+  cardClone.querySelector('.product-card__title').textContent = product.name;
+  cardClone.querySelector('.product-card__description').textContent = product.description;
+  cardClone.querySelector('.product-card__price-value').textContent = `${product.price} ₽`;
+
+  const compositionList = cardClone.querySelector('.product-card__composition-list');
+  product.composition.forEach(text => {
+    const li = document.createElement('li');
+    li.classList.add('product-card__composition-item');
+    li.textContent = text;
+    compositionList.append(li);
+  });
+
+  return cardClone;
+};
 
 const getCardsCount = () => {
   const userInput = prompt("Сколько карточек отобразить? От 1 до 5");
@@ -44,8 +47,8 @@ const renderCards = (list, count) => {
   container.innerHTML = "";
 
   slicedList.forEach(product => {
-    const cardHtml = createCardTemplate(product);
-    container.innerHTML += cardHtml;
+    const cardElement = createCardTemplate(product);
+    container.append(cardElement);
   });
 }
 
